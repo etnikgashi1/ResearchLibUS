8f0b80497b833032598e72aaf42a6d99054e7fc9
joskyday <joskyday@gmail.com>
2021-05-27 20:37:43 +0300

    smoke test suit settings

--- src/test/java/com/cybertekschool/runners/CukesRunner.java
+++ src/test/java/com/cybertekschool/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features/",
         glue = "com/cybertekschool/step_definitions",
-        tags = "@RES-79",
+        tags = "@smoke",
         dryRun = false
 )
 public class CukesRunner {
